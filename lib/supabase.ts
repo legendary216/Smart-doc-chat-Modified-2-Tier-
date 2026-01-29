37a3b741a5565281d198b1984f47db362e641efd
varad <varadsalkar216@gmail.com>
2026-01-29 22:50:22 +0530

    connected to database , api added

---
+++ lib/supabase.ts
@@ -0,0 +1,12 @@
+// lib/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase Environment Variables')
+}
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
